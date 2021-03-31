763b2db9eec5654f98fb056524fc1f76de6c335d
Lati <lati@MacBook-Pro-de-Lati.local>
2021-03-31 01:22:41 -0300

    alta y configuración inicial del proyecto

---
+++ TPfinal/src/test/java/ar/com/ada/second/online/TPfinal/LibraryTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.second.online.TPfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
